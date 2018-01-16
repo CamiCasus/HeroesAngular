8beec7cf4d093c92b60d69e9c88090e32c220745
CamiCasus <mcarbajal@sigcomt.com>
2018-01-16 18:35:30 -0500

    Avance de Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
+import { APPROUTING } from './app.routes';
+
+import { HeroesService } from './services/heroes.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavmenuComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APPROUTING
   ],
-  providers: [],
+  providers: [ HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
